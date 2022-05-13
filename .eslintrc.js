52b5bae28d37ba5cc7efb61167875d8170a3825c
Elías Heresi <elias.heresi@gmail.com>
2022-05-13 17:38:58 +0200

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
